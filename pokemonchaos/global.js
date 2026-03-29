eaf172fe3b22cc29109a2697360ff224f99ca74e
themailmangd <themailmangd@gmail.com>
2026-03-29 16:56:59 +1100

    teambuilder start

---
+++ pokemonchaos/global.js
@@ -0,0 +1,7 @@
+var format = "";
+var pkm1 = "";
+var pkm2 = "";
+var pkm3 = "";
+var pkm4 = "";
+var pkm5 = "";
+var pkm6 = "";
---
+++ pokemonchaos/pokemon/JSON/Pokemon/pokemon.js
@@ -0,0 +1,23 @@
+var archaludonms = {
+	"pkm": "archaludon",
+	"type1": "steel",
+	"type2": "dragon",
+	"hp": 90,
+	"atk": 105,
+	"def": 130,
+	"spa": 125,
+	"spd": 65,
+	"spe": 85,
+	"catchrate" : 10,
+	"evolutionlevel" : 99999,
+	"moveset" : "archaludonms.js",
+	"habitat" : [""],
+	"dex" : "When in trouble, Archaludon extends its normally bent torso, trading some of its mobility for a more stable centre of gravity. While it’s like this, the Pokémon resembles a magnificent steel bridge.",
+	"classification" : "Alloy Pokémon",
+	"height" : "6'07\"",
+	"weight" : "132.3 lbs",
+	"ability1" : "stamina",
+	"ability2" : "sturdy",
+	"hiddenability" : "stalwart"
+}
+const archaludon = JSON.parse(archaludonms);
